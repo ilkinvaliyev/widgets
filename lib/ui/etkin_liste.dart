8bbae3e0c27b8444d0b6bad66eb3e276bece481b
Ilkin Valiyev <ilkinv1994@gmail.com>
2020-12-31 20:24:41 +0400

    commit test

--- lib/ui/etkin_liste.dart
+++ lib/ui/etkin_liste.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 
-class EtkinListe extends StatelessWidget{
+class EtkinListe extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // return ListView.builder(itemBuilder: (context, index) => );
-    return null;
+    return ListView.builder(
+        itemCount: 20,
+        itemBuilder: (context, index) => Text("User : ${++index}"));
   }
+}
+
+class Student{
+
 }
